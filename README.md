d437cc95a79e7092f90be021fecf5fe9e1be9f29
Ong Yong Xin <ongyongxin2020+github@gmail.com>
2021-01-05 12:37:00 +0800

    (feat) 0.1.1a0: add last_updated timestamp, indexer version, code refactor

--- README.md
+++ README.md
@@ -1,22 +1,74 @@
 # vgmusic.py
 
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![PyPI - Downloads](https://img.shields.io/pypi/dm/vgmusic)](https://pypi.org/project/vgmusic)
+![PyPI - License](https://img.shields.io/pypi/l/vgmusic)
+![PyPI](https://img.shields.io/pypi/v/vgmusic)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/vgmusic)
+![Lines of code](https://img.shields.io/tokei/lines/github/ongyx/vgmusic.py)
+
 (unofficial) Python API for [VGMusic](vgmusic.com).
 This project is in no way affiliated with or sponsered by Mike Newman or any of the staff at VGMusic.
 
 ## Usage
+
+Thoughout these examples, we will be using the `API` object as the api:
+
 ```python
 import vgmusic
 
-# initalise the API object
-with vgmusic.VGMusic() as api:
-    # to access music info for a system, use the API as a dictionary
-    ps4 = api["Sony PlayStation 4"]
-    # get song by game title (it's a list of songs)
-    song_info = ps4["titles"]["Persona 5"][0]
-    print(song_info["sequenced_by"])  # fakt13 (shoutout :3)
+api = vgmusic.API()
 ```
 
-CLI is coming soon...
+It is recommended to close it once you are done:
+
+```python
+# do something here...
+api.close()
+```
+
+The best way is to use a [context manager](https://www.python.org/dev/peps/pep-0343/), a.k.a `with` statement:
+
+```python
+with vgmusic.API() as api:
+    # do something here
+```
+
+Note that the API is lazy: It will only retrieve data for a console/system the first time it is queried for it.
+To override this behaviour, use `force_cache` (see [Module Documentation](##module-documentation)).
+
+## Backends
+
+The API has two backends: dictionary-like (access from Python code) and a REST-based web interface (through Flask, from elsewhere).
+You can also use it from the command-line (WIP).
+
+### Dictionary/Key
+
+To query songs, you have to provide the name of the system/catagory and the game title:
+
+```python
+songs = api["Sony PlayStation 4"]["titles"]["Persona 5"]
+```
+
+You can manipulate the API using standard dictionary methods:
+
+```python
+# list all titles for a system
+titles = list(api["Nintendo Switch"].keys())  # ['Sonic Mania'], as of 5/1/2021
+# count how many songs in total
+total = sum(len(songs) for songs in api["Nintendo Switch"]["titles"].values())  # 12, as of 5/1/2021
+```
+
+Anything you can do with a dictionary, it's basically possible with this API.
+
+### REST/Flask
+
+**NOTE**: This is WIP, it has not been finished yet.
+Make sure you have installed the Flask extension:
+
+```text
+pip install vgmusic[REST]
+```
 
 ## License
 MIT.
--- vgmusic.py
+++ vgmusic.py
@@ -3,27 +3,75 @@
 
 import collections
 import json
+import locale
+import re
 import urllib.parse
+from datetime import datetime, timezone
 from typing import List, Optional
 
 import bs4
 import requests
 
-__version__ = "0.1.0a0"
+try:
+    import flask
+except ImportError:
+    flask = None
+
+__version__ = "0.1.1a0"
 
 VGMUSIC_URL = "https://vgmusic.com"
 BS4_PARSER = "html5lib"
 # parse the 'last updated' info at the end of each page
-TIMESTAMP_FORMAT = "%B %d, %Y at %I:%M %p"
+TIMESTAMP_FORMAT = "%a, %d %b %Y %H:%M:%S %Z"
+# parse the indexer version.
+RE_INDEX_VER = re.compile(r"([\d\.]{3,}[^\.])")
+
+# in case you are not in the US or using an English locale.
+locale.setlocale(locale.LC_TIME, "en_US.utf8")
 
 
-def _clean_header(name):
+def _clean_header(name: str) -> str:
     return name.lower().replace(" ", "_")
+
+
+def _parse_song_info(system_url: str, headers: List[str], row: bs4.element.Tag) -> dict:
+    song_info = {}
+    for header_name, tag in zip(headers, row.find_all("td")):
+
+        # Currently, the following headers are used on VGMusic.com.
+        # song_title: Self-explainatory.
+        # file_size: Size in bytes.
+        # sequenced_by: Who created the midi.
+        # comments: How many comments (does not work when getting HTML)
+
+        # vgmusic.py adds these headers:
+        # song_url: Direct URL to the midi file.
+        # comments_url: Direct URL to the comments page.
+
+        if header_name in ("song_title", "comments"):
+            # so we won't end up with 'song_title_url'
+            normalized = header_name.split("_")[0]
+
+            song_info[f"{normalized}_url"] = urllib.parse.urljoin(
+                system_url, tag.a["href"]
+            )
+            header_value = tag.get_text(strip=True)
+
+        elif header_name == "file_size":
+            header_value = int(tag.text.split()[0])
+
+        else:
+            header_value = tag.get_text(strip=True)
+
+        song_info[header_name] = header_value
+
+    del song_info["comments"]
+    return song_info
 
 
 # NOTE: 'system' refers to the game system (NES, SNES, etc.)
 # 'section' refers to the company that made the system (Atari, etc.)
-class VGMusic(collections.UserDict):
+class API(collections.UserDict):
     """VGMusic API.
 
     Args:
@@ -36,10 +84,13 @@ class VGMusic(collections.UserDict):
         data (dict): A map of system names to their info.
     """
 
-    def __init__(self, force_cache: Optional[List[str]] = None):
+    def __init__(
+        self,
+        force_cache: Optional[List[str]] = None,
+    ):
         self.session = requests.Session()
         # retrieve the index
-        self.soup = self._get_soup(VGMUSIC_URL)
+        self.soup, _ = self._get_soup(VGMUSIC_URL)
         # the first menu element is info, etc.
         self.soup.find("p", class_="menu").decompose()
 
@@ -48,7 +99,7 @@ class VGMusic(collections.UserDict):
         for section in self.soup.find_all("p", class_="menu"):
             section_name = section.find_previous_sibling(
                 "p", class_="menularge"
-            ).text.strip()
+            ).get_text(strip=True)
 
             # avoid namespace clash with 'system' module
             for system in section.find_all("a", href=True):
@@ -65,15 +116,32 @@ class VGMusic(collections.UserDict):
                 self.__getitem__(system)
 
     def _get_soup(self, *args, **kwargs) -> bs4.BeautifulSoup:
-        return bs4.BeautifulSoup(self.session.get(*args, **kwargs).text, BS4_PARSER)
+        response = self.session.get(*args, **kwargs)
+        return bs4.BeautifulSoup(response.text, BS4_PARSER), response
 
     def __getitem__(self, system):
         # cache it lazily
         if not self.data[system]["titles"]:
 
             system_url = self.data[system]["url"]
-            system_page = self._get_soup(system_url)
+            system_page, response = self._get_soup(system_url)
             table = system_page.tbody
+
+            # get last updated
+            self.data[system]["last_updated"] = (
+                datetime.strptime(response.headers["Last-Modified"], TIMESTAMP_FORMAT)
+                .replace(tzinfo=timezone.utc)
+                .timestamp()
+            )
+
+            # we use etag (last updated is for client-side)
+            self.data[system]["_etag"] = response.headers["ETag"]
+
+            # indexer version
+            # idk who would actually need this, but still...
+            self.data[system]["indexer_version"] = RE_INDEX_VER.findall(
+                system_page.address.get_text(strip=True)
+            )[0]
 
             # This header specifies the info on each song.
             # We just map it to the 'table row' tags below each title header onwards.
@@ -89,6 +157,7 @@ class VGMusic(collections.UserDict):
             for row in table.find_all("tr"):
 
                 if row.get("class", [""])[0] == "header":
+                    # there is no song info, skip
                     title = row.td.a.text
                     continue
 
@@ -96,44 +165,24 @@ class VGMusic(collections.UserDict):
                     # a blank row?
                     continue
 
-                song_info = {}
-
-                for header_name, tag in zip(header_names, row.find_all("td")):
-
-                    # Currently, the following headers are used on VGMusic.com.
-                    # song_title: Self-explainatory.
-                    # file_size: Size in bytes.
-                    # sequenced_by: Who created the midi.
-                    # comments: How many comments (does not work when getting HTML)
-
-                    # vgmusic.py adds these headers:
-                    # song_url: Direct URL to the midi file.
-                    # comments_url: Direct URL to the comments page.
-
-                    if header_name in ("song_title", "comments"):
-                        # so we won't end up with 'song_title_url'
-                        normalized = header_name.split("_")[0]
-
-                        song_info[f"{normalized}_url"] = urllib.parse.urljoin(
-                            system_url, tag.a["href"]
-                        )
-                        header_value = tag.text.strip()
-
-                    elif header_name == "file_size":
-                        header_value = int(tag.text.split()[0])
-
-                    else:
-                        header_value = tag.text.strip()
-
-                    song_info[header_name] = header_value
-
-                del song_info["comments"]
-                self.data[system]["titles"][title].append(song_info)
+                self.data[system]["titles"][title].append(
+                    _parse_song_info(system_url, header_names, row)
+                )
 
         return super().__getitem__(system)
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, t, v, tb):
+        self.session.close()
+
+    def as_json(self, *args, **kwargs):
+        return json.dumps(self.data, *args, **kwargs)
+
 
 if __name__ == "__main__":
-    mus = VGMusic(force_cache=["Sony PlayStation 4"])
-    with open("out.json", mode="w") as f:
-        json.dump(mus.data, f, indent=4)
+    # import argpare
+    with API(force_cache=["Sony PlayStation 4"]) as api:
+        with open("out.json", "w") as f:
+            f.write(api.as_json(indent=4))
